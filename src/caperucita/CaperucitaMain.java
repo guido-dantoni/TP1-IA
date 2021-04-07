7e421cbbd3a4629b4d1ed3437eecd997a53f7a1e
guido <guido_2345@hotmail.com>
2021-04-07 12:35:48 -0300

    agregue la clase main que contiene el simulador

--- src/caperucita/CaperucitaMain.java
+++ src/caperucita/CaperucitaMain.java
@@ -9,10 +9,10 @@ public class CaperucitaMain {
     public static void main(String[] args) throws PrologConnectorException {
     	CaperucitaAgente caperucita = new CaperucitaAgente();
         
-        Bosque pacmanEnvironment = new Bosque();
+        Bosque bosque = new Bosque();
         
         SearchBasedAgentSimulator simulator =
-                new SearchBasedAgentSimulator(pacmanEnvironment, caperucita);
+                new SearchBasedAgentSimulator(bosque, caperucita);
         
         simulator.start();
     }
