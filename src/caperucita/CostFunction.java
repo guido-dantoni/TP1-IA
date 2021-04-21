b6ce423ebb08539e66677c4eada6dc8806b4c1e7
Usser <Usser@DESKTOP-6G9E604>
2021-04-21 11:33:36 -0300

    Clases costfunction y heuristic agregadas, falta implementar sus metodos.

---
+++ src/caperucita/CostFunction.java
@@ -0,0 +1,14 @@
+package caperucita;
+
+import frsf.cidisi.faia.solver.search.IStepCostFunction;
+import frsf.cidisi.faia.solver.search.NTree;
+
+public class CostFunction implements IStepCostFunction{
+
+	@Override
+	public double calculateCost(NTree node) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/caperucita/Heuristic.java
@@ -0,0 +1,14 @@
+package caperucita;
+
+import frsf.cidisi.faia.solver.search.IEstimatedCostFunction;
+import frsf.cidisi.faia.solver.search.NTree;
+
+public class Heuristic implements IEstimatedCostFunction{
+
+	@Override
+	public double getEstimatedCost(NTree node) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
